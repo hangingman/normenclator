be3969b16ae90b3901907fc4cfd4f3e46351fd89
hiroyuki.nagata <idiotpanzer@gmail.com>
2021-07-17 13:53:36 +0900

    rebase後のビルドエラーを修正

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import BaseUI from "./components/BaseUI.vue";
-//import 'jquery-ui-dist/jquery-ui.theme.css'
 
 @Options({
   components: {
